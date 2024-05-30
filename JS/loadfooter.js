694edc604fc0629369f4c96732e2890bbf94e420
pabloperezarcos <pabloperezarcos@gmail.com>
2024-05-30 19:37:30 -0400

    Footer
    Dejamos el footer en un HTML aparte para cargarlo en todas las paginas usando JS. Esto ayuda a la mantenibilidad del mismo y ahorra lineas de texto y posibilidad de error.

---
+++ JS/loadfooter.js
@@ -0,0 +1,10 @@
+// loadFooter.js
+function loadFooter() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-container').innerHTML = data;
+        });
+}
+
+window.onload = loadFooter;
