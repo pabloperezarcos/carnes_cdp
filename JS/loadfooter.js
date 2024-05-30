0c0cfcc85f60ae70041f514d095c3428550e9013
pabloperezarcos <pabloperezarcos@gmail.com>
2024-05-30 19:50:04 -0400

    Creacion de productos
    Copiamos el formato de HTML de 1 producto para los otros.
    
    Se actualizó el footer nuevamente por problemas con la ruta de la imagen del logo.

--- JS/loadfooter.js
+++ JS/loadfooter.js
@@ -1,6 +1,6 @@
 // loadFooter.js
 function loadFooter() {
-    fetch('footer.html')
+    fetch('/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-container').innerHTML = data;
